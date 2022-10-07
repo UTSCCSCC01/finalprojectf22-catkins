32fdf18a4e44ebb7fbfdfa60d30d956303dfb20e
YoungkoonLi <i.hate.you.for.loving.myself@gmail.com>
2022-10-07 16:44:51 -0400

    CAT_4_USER_3: Removed redudent server functoin; Revised java script functions for join club; Added router for joinging club;

--- setup/src/join.js
+++ setup/src/join.js
@@ -1,6 +1,8 @@
 function form_submission()
 {
-  var club = document.getElementById("club_name").value;
+  var club = document.getElementById("myform").elements.club_name.value;
+  
   alert('You have joined ' + club);
+  
   return false;
 }
