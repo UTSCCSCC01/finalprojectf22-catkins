28d15b888f6f73dd72e98db84a7d5bc620e7cfe3
YoungkoonLi <i.hate.you.for.loving.myself@gmail.com>
2022-10-07 17:19:12 -0400

    CAT_4_USER_3: add comments for functions;

--- setup/src/join.js
+++ setup/src/join.js
@@ -1,4 +1,6 @@
+//Convert form into JSON object and set POST request
 function form_submission()
+
 {
   var content = {club_name: document.getElementById("club_name").value,
                   username: document.getElementById("username").value};//Get form info
