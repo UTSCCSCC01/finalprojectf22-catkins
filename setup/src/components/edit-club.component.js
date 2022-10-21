8be83bb37563912959be4f001e2bc0cca2d7f5e8
YoungkoonLi <i.hate.you.for.loving.myself@gmail.com>
2022-10-21 18:26:23 -0400

    Update edit-club.component.js
    Sprint 2 US 21: fixed edit_club-component.js errors"

--- setup/src/components/edit-club.component.js
+++ setup/src/components/edit-club.component.js
@@ -1,33 +1,7 @@
-import Reach from 'react';
+import Reach from "react";
 
 function EditClub() {
-  return (
-    <div className="container">
-          <div className="main">
-        <div className="image">
-            <img src="..//static/img/logo.png" alt="className logo">
-        </div>
-        <div className="form">
-            <form method="post">
-                <h2>Login</h2>
-                <div className="text">
-                    <i className="fas fa-user"></i>
-                    <input type="text" name="Username" placeholder="Username">
-                </div>
-                <div className="text">
-                    <i className="fas fa-key"></i>
-                    <input type="password" name="Password" placeholder="Password">
-                </div>
-                <input type="submit" value="Login">
-                <p><a href="/create">Create Account</a></p>
-            </form>
-            <p>Invalid username or Password</p> 
-        </div>
-    </div>
-      Hello World!'
-      <div>sdfsdf</div>
-    </div>
-  );
+  return <div className="container">Hello World!</div>;
 }
 
 export default EditClub;
