e5ba0ca5c1c48a3a254163ca008a5b1c7a7affc2
Zeneos <95008961+Zeneos@users.noreply.github.com>
2022-11-01 07:42:44 -0400

    US-12-Deleting function
    - Implemented function to delete comments and replies given post, comment, and reply ids
    
    - Changed route for comment related functions
    
    - Updated documentation

--- setup/backend/routes/feed.js
+++ setup/backend/routes/feed.js
@@ -21,7 +21,7 @@ router.route('/').get((req, res) => {
 });
 
 // Endpoint for adding a comment with a post request
-router.route('/reply').post((req, res) => {
+router.route('/comments/add').post((req, res) => {
 
     // The user in question that is commenting
     const author = req.body.author || req.query.author;
@@ -61,6 +61,49 @@ router.route('/reply').post((req, res) => {
             
         }
     });
+    
+});
+
+
+// Endpoint for deleting a comment by post
+router.route('/comments/delete').post((req, res) => {
+
+    // The post that the comment exists on
+    const parent = req.body.parent || req.query.parent;
+
+    // The comment on the post they are replying to
+    const replying = req.body.replying || req.query.replying || null;
+
+    // The id of the comment being deleted
+    const deleting = req.body.comment || req.query.comment;
+
+    // Updating post/comment
+    Post.findById(parent).then(post => {
+        if (!replying) {
+            Post.findById(parent).then((post) => {
+                for (let i = 0; i < post.comments.length; i++) {
+                    var currentComment = post.comments[i];
+                    if (currentComment._id == replying) {
+                        post.comments.splice(i, 1);
+                    }
+                }
+            }).then(() => res.json("Deleted")).catch(err => res.status(400).json('Error: ' + err));
+        } else {
+            Post.findById(parent).then((post) => {
+                for (let i = 0; i < post.comments.length; i++) {
+                    var currentComment = post.comments[i];
+                    if (currentComment._id == replying) {
+                        for (let j = 0; j < currentComment.replies.length; j++) {
+                            if (currentComments.replies[i]._id == deleting) {
+                                currentComments.replies.splice(i, 1);
+                            }
+                        }
+                    }
+                }
+            }).then(() => res.json("Deleted")).catch(err => res.status(400).json('Error: ' + err));
+            
+        }
+    }).catch(err => res.status(400).json("Error: " + err));
     
 });
 
