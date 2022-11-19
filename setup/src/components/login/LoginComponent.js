5d2a7dad8917fcb9c7065dfeb277435ec0bf90f9
IlyaKIS1 <ilyuhakostin@gmail.com>
2022-11-18 19:06:15 -0500

    US-25 created Login option in LoginComponent.js
    -Added mode state to LoginComponent.js, so that a user can switch between options to login or register
    -Added login handler on the main form of the LoginComponent.js, so now the login functionality works

--- setup/src/components/login/LoginComponent.js
+++ setup/src/components/login/LoginComponent.js
@@ -12,6 +12,8 @@ export default function LoginComponent() {
     const [userEmail, setUserEmail] = useState("");
     const [userPassword, setUserPassword] = useState("");
 
+    const [mode, setMode] = useState("Login");
+
     const [error, setError] = useState("");
 
     const onPasswordChange = (e) => {
@@ -23,7 +25,7 @@ export default function LoginComponent() {
 
     }
 
-    const onSubmit = (e) => {
+    const onSubmitRegister = (e) => {
 
         e.preventDefault();
         if (!isEmail(userEmail)){
@@ -32,8 +34,8 @@ export default function LoginComponent() {
         }
         if (!isStrongPassword(userPassword)){
             setError("Please provide a valid Password! \n\
-            it should have at least one capital letter, be 8 char length minimum \n \
-            and have at least one special character \n \
+            it should have at least one capital letter, be 8 char length minimum, \n \
+             have at least one special character and at least one number \n \
             ");
             return;
         }
@@ -43,10 +45,47 @@ export default function LoginComponent() {
             password:userPassword
         }).then(() => {
             dispatch(loginActions.login(userEmail));
-            window.localStorage.setItem("isLoggedIn", true);
+        }).catch((err) => {
+            if (err.response.status === 400) {
+                setError("This user already exists");
+            }
         })
 
     }
+
+    const onToggleMode = () => {
+        setMode(prevState => prevState === "Register" ? 'Login' : 'Register');
+    }
+
+    const onSubmitLogin = (e) => {
+
+        e.preventDefault();
+        if (!isEmail(userEmail)){
+            setError("Please provide a valid email!");
+            return;
+        }
+
+        axios.post("http://localhost:5000/api/login", {
+            username: userEmail,
+            password:userPassword
+        }).then((res) => {
+
+           if (res.status === 200) return dispatch(loginActions.login(userEmail));
+           else return setError('Uknown error happened. Try again');
+        }).catch((err) => {
+            if (err.response.data === "No user"){
+                setError('There is no such user');
+            } else if (err.response.data === 'Missing username or password'){
+                setError('Missing username or password');
+            } else if (err.response.status === 400) {
+                setError('Wrong password');
+            } else {
+                setError('Server error happened. Try again');
+            }
+        })
+
+    }
+
 
   return (
     <>
@@ -55,16 +94,24 @@ export default function LoginComponent() {
      justify-center rounded-xl items-center
      bg-opacity-10">
 
-    <form onSubmit={onSubmit} className='flex w-2/3 flex-col items-center'>
+    <form onSubmit={mode === "Register" ? onSubmitLogin : onSubmitRegister} className='flex w-2/3 flex-col items-center'>
     <label className='m-2 text-lg'>EMAIL</label>
     <input onChange={onUserNameChange} type="text" className='w-full mb-5 border border-[#211D1D] border-opacity-25'></input>
     <label className='text-lg'>PASSWORD</label>
     <input type="password" onChange={onPasswordChange} className='w-full m-2 border border-[#211D1D] border-opacity-25'></input>
-    <button type="submit" className='w-1/3 bg-[#88A1E0] mt-3 rounded-xl p-1 text-xs'>Log In</button>
+    <button type="submit" className='w-1/3 bg-[#C4E8FF] mt-3 rounded-xl p-1 text-xs'>
+    {mode === "Register" ? 'Log in' : 'Register' }
+    </button>
 
    </form>
 
+
+
    </div>
+   <button onClick={onToggleMode}
+     className='w-2/3 bg-[#C4E8FF] my-4 rounded-xl p-1 text-xs'>
+        Click here to {mode === "Register" ? 'create a new account ' : 'login to your account ' } instead
+        </button>
    <p className='text-2xl text-[#FD0808] mt-2 mx-52 text-center'>{error}</p>
 
 </>
